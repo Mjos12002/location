58bd0965c40e0a627e4cc0a81ea7d5ee667babf7
Joseph <mjos12002@gmail.com>
2025-06-13 18:18:41 +0200

    Added the pm2 dependency

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> if you want it to take effect.
         </p>
         <p>
           We are working on the <code>location</code> feature, which will allow you to manage and display locations dynamically.
