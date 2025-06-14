fee6c7a42041b93371540b644a2715e3548e17b1
Joseph <mjos12002@gmail.com>
2025-06-14 16:24:04 +0200

    Added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> if you want it to take effect.
-        </p>
-        <span>
-          It is coming right now
-        </span>
-        <p>
-          We are working on the <code>location</code> feature, which will allow you to manage and display locations dynamically.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-center text-3xl">Welcome to the Location App!</p>
     </>
   )
 }
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+var Header = () => {
+    return (
+        <div className="header">
+        <h1>Location App</h1>
+        <p>Find your favorite places</p>
+        </div>
+    )
+}
+
+export default Header
