e15d6815abf5345b6a7130d2fa1862bf7db38c2d
Joseph <mjos12002@gmail.com>
2025-06-14 17:56:53 +0200

    Added span to check the process

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -5,6 +5,7 @@ var Header = () => {
         <div className="header">
         <h1>Location App</h1>
         <p>Find your favorite places</p>
+        <span>How is it possible</span>
         </div>
     )
 }
