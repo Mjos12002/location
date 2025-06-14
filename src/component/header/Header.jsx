73bf953a5c806120f7c89ea6491940c6ab5dd79f
Joseph <mjos12002@gmail.com>
2025-06-14 18:00:16 +0200

    Added new span to check the processes

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -6,6 +6,11 @@ var Header = () => {
         <h1>Location App</h1>
         <p>Find your favorite places</p>
         <span>How is it possible</span>
+        <span>With the help of React</span>
+        <span>And the power of APIs</span>
+        <span>And the magic of JavaScript</span>
+        <span>And the beauty of CSS</span>
+        <span>And the creativity of developers</span>
         </div>
     )
 }
