49c404a99d397b0595b79ed879b1b06d43ff73c6
Joseph <mjos12002@gmail.com>
2025-06-14 17:14:31 +0200

    Added text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <>
+      <div>
+        Hear is coming the header
+      </div>
       <p className="text-center text-3xl">Welcome to the Location App!</p>
     </>
   )
